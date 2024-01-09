cae2007c4ab1395720726cb902a27706c0167dba
Onur Cayci <cayci.onur@gmail.com>
2024-01-08 19:18:45 -0500

    Initialize the Scrumdinger iOS app

---
+++ scrumdinger/scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Onur Cayci on 2024-01-08.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
